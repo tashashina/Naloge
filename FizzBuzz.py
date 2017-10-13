58ef6ae002ecc85b429bc7deea4d5b43538e8d3b
Natasa Anzelj <natasa.fekonja@gmail.com>
2017-10-13 22:21:51 +0200

    *Druga naloga nova

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,12 +1,12 @@
 number = int(raw_input("Vnesi stevilo med 1 in 100: "))
 
-for i in range(number):
+for i in range(1, number+1):
 
-    if ((i+1) % 3 == 0) and ((i+1) % 5 != 0):
+    if (i % 3 == 0) and (i % 5 != 0):
         print("fizz")
-    elif((i+1) % 3 != 0) and ((i+1) % 5 == 0):
+    elif(i % 3 != 0) and (i % 5 == 0):
         print("buzz")
-    elif ((i+1)%3==0) and ((i+1)%5==0):
+    elif (i % 3 == 0) and (i % 5 == 0):
         print("fizzbuzz")
     else:
-        print(i+1)
+        print(i)
