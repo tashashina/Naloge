09be6d528720fbaf3fe9d29cb9830fd63b7c17b6
Natasa Anzelj <natasa.fekonja@gmail.com>
2017-10-24 06:32:56 +0200

    *Naloga funkcije in testiranje

--- UganiSkritoStevilo.py
+++ UganiSkritoStevilo.py
@@ -1,12 +1,32 @@
-secret = 7
+import random
 
-guess = raw_input("Ugani celo stevilo: ")
 
-if guess.isdigit():
-    guess = int(guess)
-    if guess == secret:
-        print("Cestitam, uganil si stevilo!")
-    else:
-        print("Nisi uganil stevila!")
-else:
-    print("Nisi vnesel celega stevila! ")
+def main():
+    secret = random.randint(1, 30)
+    print ("Ugani celo stevilo od 1 do 30")
+
+    while True:
+        guess = raw_input("Vnesi stevilo: ")
+
+        if guess.isdigit():
+            guess = int(guess)
+            if guess == secret:
+                print("Cestitam, uganil si stevilo!")
+            else:
+                print("Nisi uganil stevila!")
+        else:
+            print("Nisi vnesel celega stevila! ")
+
+        new = raw_input("Zelis nadaljevati? (Da/Ne): ")
+
+        if new.lower() == "da":
+            continue
+        else:
+            print ("Iskano stevilo je " + str(secret))
+            break
+
+    print "END"
+
+
+if __name__ == "__main__":
+    main()
